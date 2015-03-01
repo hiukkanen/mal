84a8b5cc45f59b4a86b41c7c822c0a47c8117469
Joni Lahtinen <joni.lahtinen@gmail.com>
2015-03-01 23:21:11 +0200

    Introduce Mal class

--- ruby/step3_env.rb
+++ ruby/step3_env.rb
@@ -4,66 +4,68 @@ require_relative 'printer'
 require_relative 'env'
 require_relative 'repl_exception'
 
-def prompt
-  "mal-user> "
-end
-
-def READ(value)
-  Reader.read_str(value)
-end
-
-def eval_ast(ast, env)
-  return env.get(ast) if ast.is_a? Symbol
-  return ast.map { |value| EVAL(value, env) } if ast.is_a? Array
-  ast
-end
-
-def new_env(ast, env)
-  lets = ast[1]
-  new_env = Env.new(env)
-  lets.each_slice(2).each do |symbol, value|
-    new_env.set(symbol, value)
+class Mal
+  def prompt
+    "mal-user> "
   end
-  EVAL(ast[2], new_env)
-end
 
-def post_eval(asts, env)
-  asts.map do |ast|
-    if ast.is_a? Array
-      EVAL(ast, env)
-    else
-      ast
+  def READ(value)
+    Reader.read_str(value)
+  end
+
+  def eval_ast(ast, env)
+    return env.get(ast) if ast.is_a? Symbol
+    return ast.map { |value| EVAL(value, env) } if ast.is_a? Array
+    ast
+  end
+
+  def new_env(ast, env)
+    lets = ast[1]
+    new_env = Env.new(env)
+    lets.each_slice(2).each do |symbol, value|
+      new_env.set(symbol, value)
+    end
+    EVAL(ast[2], new_env)
+  end
+
+  def post_eval(asts, env)
+    asts.map do |ast|
+      if ast.is_a? Array
+        EVAL(ast, env)
+      else
+        ast
+      end
     end
   end
-end
 
-def EVAL(ast, env)
-  return env.set(ast[1], EVAL(ast[2], env)) if ast[0] == :def!
-  return new_env(ast, env) if ast[0] == :"let*"
-  return eval_ast(ast, env) unless ast.is_a? Array
-  evaluation = eval_ast(ast, env)
-  evaluation.first.call(*(post_eval(evaluation[1..-1], env)))
-end
+  def EVAL(ast, env)
+    return env.set(ast[1], EVAL(ast[2], env)) if ast[0] == :def!
+    return new_env(ast, env) if ast[0] == :"let*"
+    return eval_ast(ast, env) unless ast.is_a? Array
+    evaluation = eval_ast(ast, env)
+    evaluation.first.call(*(post_eval(evaluation[1..-1], env)))
+  end
 
-def PRINT(ast)
-  Printer.new.pr_str(ast)
-end
+  def PRINT(ast)
+    Printer.new.pr_str(ast)
+  end
 
-def rep(value)
-  PRINT(EVAL(READ(value), repl_env))
-rescue ReplException => e
-  puts e.message
-end
+  def rep(value)
+    PRINT(EVAL(READ(value), repl_env))
+  rescue ReplException => e
+    puts e.message
+  end
 
-def repl_env
-  @env ||= Env.new.tap do |env|
-    {
-      "+": -> (a, b) { a + b },
-      "-": -> (a, b) { a - b },
-      "*": -> (a, b) { a * b },
-      "/": -> (a, b) { a / b }
-    }.each do |key, value|
-      env.set(key, value)
+  def repl_env
+    @env ||= Env.new.tap do |env|
+      {
+        "+": -> (a, b) { a + b },
+        "-": -> (a, b) { a - b },
+        "*": -> (a, b) { a * b },
+        "/": -> (a, b) { a / b }
+      }.each do |key, value|
+        env.set(key, value)
+      end
     end
   end
 end
@@ -74,7 +76,9 @@ trap('INT') do
   exit
 end
 
-while line = Readline.readline(prompt, true) do
-  puts (rep line)
+mal = Mal.new
+
+while line = Readline.readline(mal.prompt, true) do
+  puts (mal.rep line)
 end
 puts ""
